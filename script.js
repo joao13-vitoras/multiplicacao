3f32b2e5917f4dae74c61448a920e9f3497ce0f8
joao13-vitoras <70807079+joao13-vitoras@users.noreply.github.com>
2020-11-01 12:26:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function multiplicacao (a, b) {
+    return a * b
+}
+let x = parseInt(prompt('Digite um número:'));
+let y = parseInt(prompt('Digite um número:'));
+
+alert ('A multiplicação resultou em ' + multiplicacao(x, y))
